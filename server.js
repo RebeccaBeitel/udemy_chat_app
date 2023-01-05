2bdf3d44eecc1f390e600eef8373f4b98613bfc5
RebeccaBeitel <rebecca.todd@foretify.ai>
2023-01-05 08:04:54 -0800

    Set up users Get Route and installed modules

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const http = require("http");
+const cookieParser = require("cookie-parser");
+const validator = require("express-validator");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const flash = require("flash");
+const passport = require("passport");
+
 const container = require("./container");
-const { Router } = require("express");
 
 container.resolve(function (users) {
+  mongoose.Promise = global.Promise;
+  mongoose.connect("mongodb://localhost/udemy_chat_app", {
+    useMongoClient: true,
+  });
   const app = SetupExpress();
   function SetupExpress() {
     const app = express();
-    const sever = http.createServer(app);
+    const server = http.createServer(app);
     server.listen(3000, function () {
       console.log("Listening on port 3000");
     });
@@ -24,8 +35,22 @@ container.resolve(function (users) {
 
   function ConfigureExpress(app) {
     app.use(express.static("public"));
+    app.use(cookieParser());
     app.set("view engine", "ejs");
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(validator());
+    app.use(
+      session({
+        secret: "thisisasecretkey",
+        resave: true,
+        saveInitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      })
+    );
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
   }
 });
